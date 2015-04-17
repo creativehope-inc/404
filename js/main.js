d6700c7d8522eb2087fdb45c6ffbde5cb22c9f4b
mgoldchild <mgoldchild@gmail.com>
2015-04-17 19:41:51 +0900

    エラーブラウザ機能追加

--- js/main.js
+++ js/main.js
@@ -117,8 +117,7 @@ $( function(){
 				}
 				// スペースでリトライ機能
 				if ( game.input.space ) {
-					console.log('ゲーム押されました');
-					store.currentScene = 'gamestart'
+					store.currentScene = 'gamestart';
 				}
 			}
 		);
@@ -801,6 +800,49 @@ $( function(){
 	// ####################
 	game.onload = main;
 	game.start();
+
+	// ####################
+	//   非対応処理
+	// ####################
+	var userAgent = window.navigator.userAgent.toLowerCase();
+	var appVersion = window.navigator.appVersion.toLowerCase();
+
+	var ua = (function() {
+		if (userAgent.indexOf('opr') != -1) {
+		  return 'opera';
+		} else if (userAgent.indexOf('msie') != -1) {
+			// IEの場合は判定
+			if (appVersion.indexOf("msie 6.") != -1) {
+				return 'ie6';
+			} else if (appVersion.indexOf("msie 7.") != -1) {
+				return 'ie7';
+			} else if (appVersion.indexOf("msie 8.") != -1) {
+				return 'ie8';
+			} else if (appVersion.indexOf("msie 9.") != -1) {
+				return 'ie9';
+			} else {
+				return 'ie';
+			}
+		} else if (userAgent.indexOf('chrome') != -1) {
+			return 'chrome';
+		} else if (userAgent.indexOf('safari') != -1) {
+			return 'safari';
+		} else if (userAgent.indexOf('gecko') != -1) {
+			return 'gecko';
+		} else {
+			return false;
+		}
+	}());
+
+	// 要素の削除
+	if (ua =='ie7' || ua == 'ie6' || ua == 'ie8' || ua == 'opera') {
+		game.stop();
+		$(function(){
+			console.log('GWEgew');
+			$("#enchant-stage").remove();
+			$('.ie_alert').show().css("text-aline", 'center');
+		});
+	}
 
 
 } );
